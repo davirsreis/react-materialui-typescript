bcaaf007c645728c0c3dbbb25269741fa7794b6a
davirsreis <daviresende.2312@gmail.com>
2023-03-16 17:57:48 -0300

    upload files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { useDrawerContext } from '../shared/contexts';
-import { Dashboard } from '../pages';
+import { Dashboard, ListagemDePessoas, DetalheDePessoas, ListagemDeCidades, DetalheDeCidades } from '../pages';
 
 export const AppRoutes = () => {
 
@@ -15,12 +15,29 @@ export const AppRoutes = () => {
         path: '/pagina-inicial',
         label: 'Página inicial',
       },
+      {
+        icon: 'location_city',
+        path: '/cidades',
+        label: 'Cidades',
+      },
+
+      {
+        icon: 'people',
+        path: '/pessoas',
+        label: 'Pessoas',
+      },
     ]);
   }, []);
 
   return(
     <Routes>
       <Route path='/pagina-inicial' element={<Dashboard />} />
+
+      <Route path='/pessoas' element={<ListagemDePessoas />} />
+      <Route path='/pessoas/detalhe/:id' element={<DetalheDePessoas />} />
+
+      <Route path='/cidades' element={<ListagemDeCidades />} />
+      <Route path='/cidades/detalhe/:id' element={<DetalheDeCidades />} />
 
       <Route path='*' element={<Navigate to="/pagina-inicial" />} />
 
